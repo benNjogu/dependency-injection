99b39a4e7bf66af085a173671bf99c513b8e7558
benNjogu <kamandemachafi.gmail.com>
2021-10-02 14:53:26 +0300

    trying unit testing

---
+++ di-demo/src/test/java/com/keytech/didemo/Controllers/ConstructorInjectorTest.java
@@ -0,0 +1,14 @@
+package com.keytech.didemo.Controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ConstructorInjectorTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
