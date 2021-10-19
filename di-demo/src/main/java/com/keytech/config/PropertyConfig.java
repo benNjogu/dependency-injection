990307ed660b5b539cabb116de13d0b0cad7acc4
benNjogu <kamandemachafi.gmail.com>
2021-10-19 11:20:49 +0300

    Accessing data from the OS level.

--- di-demo/src/main/java/com/keytech/config/PropertyConfig.java
+++ di-demo/src/main/java/com/keytech/config/PropertyConfig.java
@@ -1,10 +1,12 @@
 package com.keytech.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 import com.keytech.didemo.ExampleBeans.FakeDataSource;
 
@@ -12,6 +14,9 @@ import com.keytech.didemo.ExampleBeans.FakeDataSource;
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
 
+	@Autowired
+	Environment env;
+	
 	@Value("${keyman.username}")
 	String user;
 	@Value("${keyman.password}")
@@ -22,7 +27,7 @@ public class PropertyConfig {
 	@Bean
 	public FakeDataSource fakeDataSource() {
 		FakeDataSource fakeDataSource = new FakeDataSource();
-		fakeDataSource.setUser(user);
+		fakeDataSource.setUser(env.getProperty("USERNAME"));
 		fakeDataSource.setPassword(password);
 		fakeDataSource.setUrl(url);
 		return fakeDataSource;
