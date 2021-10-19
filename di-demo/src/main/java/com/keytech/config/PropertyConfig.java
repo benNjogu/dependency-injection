bdf489645ff21723cccf1769e6ef1ce315f24bca
benNjogu <kamandemachafi.gmail.com>
2021-10-19 20:18:09 +0300

    changing annotation

--- di-demo/src/main/java/com/keytech/config/PropertyConfig.java
+++ di-demo/src/main/java/com/keytech/config/PropertyConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
@@ -12,27 +13,30 @@ import com.keytech.didemo.ExampleBeans.FakeDataSource;
 import com.keytech.didemo.ExampleBeans.FakeJMSBroker;
 
 @Configuration
-@PropertySource({"classpath:datasource.properties","classpath:jms.properies"})
+//@PropertySource({"classpath:datasource.properties","classpath:jms.properies"})
+@PropertySources({
+		@PropertySource("classpath:datasource.properties"),
+		@PropertySource("classpath:jms.properies")
+})
 public class PropertyConfig {
 
 	@Autowired
 	Environment env;
-	
+
 	@Value("${keyman.username}")
 	String user;
 	@Value("${keyman.password}")
 	String password;
 	@Value("${keyman.dburl}")
 	String url;
-	
-	
+
 	@Value("${keyman.jms.username}")
 	String jmsuser;
 	@Value("${keyman.jms.password}")
 	String jmspassword;
 	@Value("${keyman.jms.dburl}")
 	String jmsurl;
-	
+
 	@Bean
 	public FakeDataSource fakeDataSource() {
 		FakeDataSource fakeDataSource = new FakeDataSource();
@@ -41,21 +45,21 @@ public class PropertyConfig {
 		fakeDataSource.setUrl(url);
 		return fakeDataSource;
 	}
-	
+
 	@Bean
 	public FakeJMSBroker fakeJMSBroker() {
 		FakeJMSBroker fakeJMSBroker = new FakeJMSBroker();
 		fakeJMSBroker.setUserName(user);
 		fakeJMSBroker.setPassword(password);
 		fakeJMSBroker.setUrl(url);
-		
+
 		return fakeJMSBroker;
 	}
-	
+
 	@Bean
 	public static PropertySourcesPlaceholderConfigurer properties() {
 		PropertySourcesPlaceholderConfigurer placeholderConfigurer = new PropertySourcesPlaceholderConfigurer();
 		return placeholderConfigurer;
 	}
-	
+
 }
